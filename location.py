303dbad4e05afd0e3685d1245f8ca3357b25e92d
Ethan McCue <ethan_mccue@intuit.com>
2018-06-10 11:12:05 -0700

    cleaned up location functions

--- location.py
+++ location.py
@@ -1,17 +1,24 @@
-"""Helper functions for the Cartesian location hacking that is for some reason
-sprinkled everywhere."""
+"""Helper functions for the Cartesian location hacking that is sprinkled everywhere."""
 
-def makeLocCartesian(Loc):
-    returner = []
-    locDic = {"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8}
+def makeLocCartesian(loc):
+    locDic = { "A": 1,
+               "B": 2,
+               "C": 3,
+               "D": 4,
+               "E": 5,
+               "F": 6,
+               "G": 7,
+               "H": 8 }
+    return [locDic[loc[0]], int(loc[1])]
 
-    returner.append(locDic[Loc[0]])
-    returner.append(int(Loc[1]))
-    return returner
+def makeLocAlphaNumeric(loc):
+    locDic = { 1: 'A',
+               2: 'B',
+               3: 'C',
+               4: 'D',
+               5: 'E',
+               6: 'F',
+               7: 'G',
+               8: 'H' }
 
-def makeLocAlphaNumeric(Loc):
-    locDic = {1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'}
-    returner = ""
-    returner+=(locDic[Loc[0]])
-    returner+=str(Loc[1])
-    return returner
+    return "" + locDic[loc[0]] + str(loc[1])
