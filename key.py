8ceffbaf3f71e378eed873c13357479140cd5186
Ethan McCue <emccue@live.com>
2018-07-15 13:07:46 -0400

    remove unused property

--- key.py
+++ key.py
@@ -2,15 +2,11 @@
 #All rights reserved to MacaulayCore
 
 class Key:
-    def __init__(self, location, direction, isLocked, team, selected=False):
+    def __init__(self, location, direction, isLocked, team):
         self.location = location
         self.direction = direction
         self.isLocked = isLocked
         self.team = team
-        self.isSelected = selected
 
     def lock(self):
         self.isLocked = True
-
-    def select(self):
-        self.isSelected = not self.isSelected
